0e8734032429da40bf076720a4d92075c0bc9e4d
JiSolBin <solbin0027@naver.com>
2022-05-26 18:04:40 +0900

    220526 update Embeded tomcat server
    context 추가해서 경로 설정

--- day02/src/main/java/com/bit/day02/MyTomcat.java
+++ day02/src/main/java/com/bit/day02/MyTomcat.java
@@ -1,26 +1,29 @@
 package com.bit.day02;
 
-//import org.apache.catalina.Server;
-//import org.apache.catalina.startup.Tomcat;
-//
-//import javax.servlet.http.HttpServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.Server;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.http.HttpServlet;
 
 
 public class MyTomcat {
 
 	public static void main(String[] args) {
-//		Tomcat serve = new Tomcat();
-//		serve.setPort(8080);
-//		try {
-//			serve.addWebapp("/", "C:\\BitFramework\\day02\\src\\main\\webapp");
-//			serve.addServlet("", "/ex01", (HttpServlet)Class.forName("com.bit.controller.Ex01Controller").newInstance());
-//			
-//			serve.start();
-//			Server server = serve.getServer();
-//			server.await();
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		Tomcat serve = new Tomcat();
+		serve.setPort(8080);
+		try {
+			Context cont = serve.addContext("/", "C:\\BitFramework\\day02\\src\\main\\webapp");
+			serve.addWebapp("/", "C:\\BitFramework\\day02\\src\\main\\webapp");
+			serve.addServlet(cont, "/ex01", (HttpServlet)Class.forName("com.bit.controller.Ex01Controller").newInstance());
+			cont.addServletMapping("/ex01", "/ex01");
+			
+			serve.start();
+			Server server = serve.getServer();
+			server.await();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
