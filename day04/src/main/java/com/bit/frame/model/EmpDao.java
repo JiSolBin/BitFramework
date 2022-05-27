230800128a63bf5b7a51e7307e669c9c781dc5d6
JiSolBin <solbin0027@naver.com>
2022-05-27 17:29:55 +0900

    220527 Dao 모듈화
    template method design pattern

--- day04/src/main/java/com/bit/frame/model/EmpDao.java
+++ day04/src/main/java/com/bit/frame/model/EmpDao.java
@@ -4,70 +4,85 @@ import java.sql.*;
 import java.util.*;
 
 import com.bit.frame.model.entity.EmpVo;
+import com.bit.frame.util.QueryTemplate;
+import com.bit.frame.util.UpdateTemplate;
 
 public class EmpDao {
 
 	public List<EmpVo> selectAll() throws SQLException {
 
-		List<EmpVo> list = new ArrayList<>();
 		String sql = "select * from emp";
-
-		String driver = "com.mysql.cj.jdbc.Driver";
-		String url = "jdbc:mysql://localhost:3306/scott";
-		String user = "user01";
-		String password = "1234";
-
-		try {
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn = DriverManager.getConnection(url, user, password);
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
+		QueryTemplate template = new QueryTemplate() {
+			
+			@Override
+			public Object rowMapper(ResultSet rs) throws SQLException {
 				EmpVo bean = new EmpVo();
 				bean.setEmpno(rs.getInt("empno"));
 				bean.setEname(rs.getString("ename"));
 				bean.setSal(rs.getInt("sal"));
 				bean.setJob(rs.getString("job"));
-				list.add(bean);
+				
+				return bean;
 			}
-		} finally {
-			close(conn, pstmt, rs);
-		}
-
-		return list;
+		};
+		
+		template.setConn(getConnection());
+		return template.executeQuery(sql);
 	}
 
 	public void insertOne(int empno, String ename, int sal, String job) throws SQLException {
 
 		String sql = "insert into emp (empno,ename,sal,job) values(?,?,?,?)";
-		executeUpdate(sql, new Object[] { empno, ename, sal, job });
+		UpdateTemplate template = new UpdateTemplate() {
+			
+			@Override
+			public void setStatement(PreparedStatement pstmt) throws SQLException {
+				pstmt.setInt(1, empno);
+				pstmt.setString(2, ename);
+				pstmt.setInt(3, sal);
+				pstmt.setString(4, job);
+			}
+		};
+		
+		template.setConn(getConnection());
+		template.executeUpdate(sql);
+	}
+	
+	public void deleteOne(int empno) throws SQLException {
+
+		String sql = "delete from emp where empno=?";
+		UpdateTemplate template = new UpdateTemplate(getConnection()) {
+			
+			@Override
+			public void setStatement(PreparedStatement pstmt) throws SQLException {
+				pstmt.setInt(1, empno);
+			}
+		};
+		
+		template.executeUpdate(sql);
 	}
 
-	public void executeUpdate(String sql, Object[] arr) throws SQLException {
+	public Object selectOne(int empno) throws SQLException {
 
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			for (int i = 0; i < arr.length; i++)
-				pstmt.setObject(i + 1, arr[i]);
-			pstmt.executeUpdate();
-		} finally {
-			close(conn, pstmt, null);
-		}
+		String sql = "select * from emp where empno="+empno;
+		QueryTemplate template = new QueryTemplate() {
+			
+			@Override
+			public Object rowMapper(ResultSet rs) throws SQLException {
+				EmpVo bean = new EmpVo();
+				bean.setEmpno(rs.getInt("empno"));
+				bean.setEname(rs.getString("ename"));
+				bean.setSal(rs.getInt("sal"));
+				bean.setJob(rs.getString("job"));
+				
+				return bean;
+			}
+		};
+		
+		template.setConn(getConnection());
+		return (EmpVo) template.queryForObject(sql);
 	}
-
+	
 	public Connection getConnection() throws SQLException {
 
 		String driver = "com.mysql.cj.jdbc.Driver";
@@ -83,7 +98,7 @@ public class EmpDao {
 
 		return DriverManager.getConnection(url, user, password);
 	}
-
+	
 	public void close(Connection conn, PreparedStatement pstmt, ResultSet rs) throws SQLException {
 
 		if (rs != null)
@@ -94,9 +109,4 @@ public class EmpDao {
 			conn.close();
 	}
 
-	public void deleteOne(int empno) throws SQLException {
-
-		String sql = "delete from emp where empno=?";
-		executeUpdate(sql, new Object[] { empno });
-	}
 }
