3799ea043cce0744704be1cbe79428dfa56ede78
JiSolBin <solbin0027@naver.com>
2022-05-31 11:01:28 +0900

    220531 spring project init
    sts01-servlet.xml 만들어서 mapping

---
+++ sts01/src/test/java/com/bit/frame/WelcomeTest.java
@@ -0,0 +1,16 @@
+package com.bit.frame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WelcomeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
