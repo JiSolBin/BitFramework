15122f5c1d95a645aeea6501cdd250e862ae4a59
JiSolBin <solbin0027@naver.com>
2022-06-08 18:05:45 +0900

    220608 Transaction xml, annotation

--- sts05/src/main/java/com/bit/sts05/service/EmpService.java
+++ sts05/src/main/java/com/bit/sts05/service/EmpService.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 
 import com.bit.sts05.model.EmpVo;
 
+//이 인터페이스 상속받는 모든 클래스에 트랜잭션 처리
+//@Transactional(rollbackFor=SQLException.class)
 public interface EmpService {
 
 	void selectAll(Model model) throws SQLException;
--- sts05/src/main/java/com/bit/sts05/service/EmpServiceImpl.java
+++ sts05/src/main/java/com/bit/sts05/service/EmpServiceImpl.java
@@ -4,11 +4,14 @@ import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
 import com.bit.sts05.model.EmpDao;
 import com.bit.sts05.model.EmpVo;
 
+//여기에 붙으면 이 클래스에 속한 모든 메서드에 트랜잭션 처리
+//@Transactional(rollbackFor=SQLException.class)
 @Service
 public class EmpServiceImpl implements EmpService {
 
@@ -21,9 +24,15 @@ public class EmpServiceImpl implements EmpService {
 		model.addAttribute("list", empDao.findAll());
 	}
 	
+	//여기에 붙으면 이 메서드만 트랜잭션 처리
+	@Transactional(rollbackFor=SQLException.class)
 	@Override
 	public void insert(EmpVo bean) throws SQLException {
 		
+		empDao.insertOne(bean);
+		
+		// 두 번 입력 되도록
+		bean.setEmpno(bean.getEmpno()+1);
 		empDao.insertOne(bean);
 	}
 }
