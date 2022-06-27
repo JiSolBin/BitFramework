f2561371248d2cd9f32390ada8af3902360ec932
JiSolBin <solbin0027@naver.com>
2022-06-27 14:01:41 +0900

    220627 Spring Security Architecture (JDBC)
    https://spring.io/guides/topicals/spring-security-architecture/

---
+++ boot12/src/main/java/com/bit/sts30/Boot12Application.java
@@ -0,0 +1,13 @@
+package com.bit.sts30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot12Application.class, args);
+	}
+
+}
---
+++ boot12/src/main/java/com/bit/sts30/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.bit.sts30.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// "/", "/login", "/logout" 만 접근 가능하고, 나머지 요청은 인증이 완료되어야 접근 가능
+		http.authorizeHttpRequests().antMatchers("/", "/login", "/logout").permitAll();
+		
+		// "/hello", "/api/emp/**" 로 접근하려면 인증해야 됨
+		// http.authorizeHttpRequests().antMatchers("/hello").authenticated();
+		// http.authorizeHttpRequests().antMatchers("/api/emp/**").authenticated();
+		// 위 세 개 제외 어디든 접근하려면 인증
+		// http.authorizeHttpRequests().anyRequest().authenticated();
+		// 위 세 개 제외 어디든 접근하려면 "ADMIN" 권한 이어야 함
+		// http.authorizeHttpRequests().anyRequest().hasRole("ADMIN");
+		
+		http.authorizeHttpRequests().anyRequest().hasAnyRole("ADMIN", "USER");
+		http.formLogin().loginPage("/login");
+	}
+
+	@Autowired
+	DataSource dataSource;
+
+//	@Override
+//	public void configure(AuthenticationManagerBuilder builder) throws Exception {
+//		// insert username:dave/pw:1234
+//		// 권한:USER
+//		builder.jdbcAuthentication().dataSource(dataSource)
+//			.withUser("admin").password(getPasswordEncoder().encode("1234")).roles("ADMIN");
+//	}
+	
+	@Bean
+	BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select username, password, enabled from users where username=?")
+			.authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+		;
+	}
+}
